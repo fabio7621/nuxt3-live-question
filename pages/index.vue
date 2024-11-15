0f21eeb23af2e60aad0a4e4bb3b1f2dee774a4cb
fabio7621 <d6200121a@gmail.com>
2024-11-15 15:06:20 +0800

    finish homework

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,7 @@
 <script lang="ts" setup></script>
 
 <template>
-  <div>
-    Page: 首頁
-  </div>
+  <div>Page: 首頁<button type="button" class="btn bg-info">TEST BTN</button></div>
 </template>
 
 <style scoped></style>
