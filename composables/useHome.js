4a3b5e546f193923bf09f8f5f628499ebd1573d4
fabio7621 <d6200121a@gmail.com>
2024-11-17 16:48:57 +0800

    finish

--- composables/useHome.js
+++ composables/useHome.js
@@ -1,3 +1,20 @@
 export const useHome = () => {
-  return {};
+  const newsList = ref([]);
+  const isLoading = ref(false);
+
+  const getNews = async () => {
+    const url = `https://nuxr3.zeabur.app/api/v1/home/news/`;
+    isLoading.value = true;
+
+    const res = await useFetch(url);
+    newsList.value = res.data.value.result;
+
+    isLoading.value = false;
+  };
+
+  return {
+    newsList,
+    isLoading,
+    getNews,
+  };
 };
