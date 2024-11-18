81e25b78996102b284653076d6fa811d6cdf73e6
fabio7621 <d6200121a@gmail.com>
2024-11-18 15:50:55 +0800

    finish

--- pages/register.vue
+++ pages/register.vue
@@ -1,26 +1,40 @@
 <script setup>
 const { $swal } = useNuxtApp();
-
-// 表單格式
 const userRegisteObject = ref({
-  name: "",
   email: "",
   password: "",
-  phone: "",
-  birthday: "",
-  address: {
-    zipcode: "",
-    detail: "",
-  },
+  nickname: "",
 });
-// 使用 sweetAlert2 套件顯示訊息
-// $swal.fire({
-//   position: "center",
-//   icon: ...,
-//   title: ...,
-//   showConfirmButton: false,
-//   timer: 1500,
-// });
+
+const processRegistration = async (requsetBody) => {
+  try {
+    const response = await $fetch("/users/sign_up", {
+      method: "POST",
+      baseURL: "https://todolist-api.hexschool.io/",
+      body: {
+        ...requsetBody,
+      },
+    });
+    await $swal.fire({
+      position: "center",
+      icon: "success",
+      title: "註冊成功",
+      showConfirmButton: false,
+      timer: 1500,
+    });
+  } catch (error) {
+    const { message } = error.response._data;
+    await $swal.fire({
+      position: "center",
+      icon: "error",
+      title: message,
+      showConfirmButton: false,
+      timer: 1500,
+    });
+  } finally {
+    userRegisteObject.value = { address: {} };
+  }
+};
 </script>
 
 <template>
@@ -30,18 +44,10 @@ const userRegisteObject = ref({
         <div class="col-12 col-md-11 col-lg-8 col-xl-7 col-xxl-6">
           <div class="bg-white p-4 p-md-5 rounded shadow-sm">
             <h2 class="h3 mb-4">會員註冊</h2>
-            <form @submit.prevent="">
+            <form @submit.prevent="processRegistration(userRegisteObject)">
               <div class="form-floating mb-4">
-                <input
-                  type="text"
-                  class="form-control"
-                  id="firstName"
-                  placeholder="王小明"
-                  required
-                />
-                <label for="firstName"
-                  >姓名 <span class="text-danger">*</span></label
-                >
+                <input type="text" class="form-control" id="firstName" placeholder="王小明" required v-model="userRegisteObject.nickname" />
+                <label for="firstName">姓名 <span class="text-danger">*</span></label>
               </div>
 
               <div class="form-floating mb-4">
@@ -50,12 +56,11 @@ const userRegisteObject = ref({
                   class="form-control"
                   id="email"
                   placeholder="example@gmail.com"
-                  pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$"
+                  pattern="^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$"
                   required
+                  v-model="userRegisteObject.email"
                 />
-                <label for="email"
-                  >信箱 <span class="text-danger">*</span></label
-                >
+                <label for="email">信箱 <span class="text-danger">*</span></label>
               </div>
 
               <div class="form-floating mb-4">
@@ -66,65 +71,12 @@ const userRegisteObject = ref({
                   placeholder="請輸入 8 碼以上密碼"
                   pattern=".{8,}"
                   required
+                  v-model="userRegisteObject.password"
                 />
-                <label for="password"
-                  >密碼 <span class="text-danger">*</span></label
-                >
+                <label for="password">密碼 <span class="text-danger">*</span></label>
               </div>
 
-              <div class="form-floating mb-4">
-                <input
-                  type="tel"
-                  class="form-control"
-                  id="phone"
-                  placeholder="0912345678"
-                  pattern="(\+886|0)?9\d{8}|(\+886|0)?2\d{8}|\d{3}-\d{4}-\d{4}"
-                  required
-                />
-                <label for="phone">電話</label>
-              </div>
-
-              <div class="form-floating mb-4">
-                <input
-                  type="date"
-                  class="form-control"
-                  id="dateInput"
-                  required
-                />
-                <label for="dateInput">出生年月日</label>
-              </div>
-
-              <div class="row">
-                <div class="col-md-6">
-                  <div class="form-floating mb-4">
-                    <input
-                      type="text"
-                      class="form-control"
-                      id="zipcode"
-                      placeholder="100"
-                      pattern="\d{3,}"
-                      required
-                    />
-                    <label for="zipcode">郵遞區號</label>
-                  </div>
-                </div>
-                <div class="col-md-6">
-                  <div class="form-floating mb-4">
-                    <input
-                      type="text"
-                      class="form-control"
-                      id="address"
-                      placeholder="台北市中正區重慶南路一段"
-                      required
-                    />
-                    <label for="address">詳細地址</label>
-                  </div>
-                </div>
-              </div>
-
-              <button class="btn btn-lg btn-primary w-100" type="submit">
-                註冊
-              </button>
+              <button class="btn btn-lg btn-primary w-100" type="submit">註冊</button>
             </form>
           </div>
         </div>
